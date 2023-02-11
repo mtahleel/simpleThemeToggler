9d756c0612439222e1c5f96b0dc39952e965f8d3
tahleel <mtahleel11@gmail.com>
2023-02-11 17:29:40 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import "./styles.css";
+var btn = document.getElementById("btn");
+var span = document.getElementById("span");
+// var app = document.getElementById("app");
+var body = document.getElementById("body");
+var toggle = false;
+
+btn.addEventListener("click", function () {
+  if (!toggle) {
+    body.style.backgroundColor = "black";
+    body.style.color = "white";
+    span.style = "left:auto; right:-10%;";
+    btn.style = "box-shadow: 0 0 10px 2px white";
+    toggle = true;
+  } else {
+    body.style.backgroundColor = "white";
+    body.style.color = "black";
+    span.style = "right:auto; left:15%;";
+    btn.style = "box-shadow: 0 0 10px 2px black";
+    toggle = false;
+  }
+});
